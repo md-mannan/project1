1c656be3c75c47fb44d3c71661d8cce34a5a4148
md-mannan <mdmannan580@gmail.com>
2024-07-24 13:28:01 +0300

    Seed data from json file

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Student;
+use Illuminate\Support\Facades\File;
 
 class StudentSeeder extends Seeder
 {
@@ -13,31 +14,20 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        $students = collect([
-            [
-                "name" => "Mohammad Abdul Mannan",
-                "email" => "mdmannan@gmail.com",
-                "age" => 27,
-                "gender" => "Male",
-                "country" => "Bangladesh",
-                "city" => "Comilla",
-                "address" => "Monohorgonj,Comilla",
-                "phone" => "50923840",
-            ],
-            [
-                "name" => "Ravi Raj",
-                "email" => "ravi@gmail.com",
-                "age" => 28,
-                "gender" => "Male",
-                "country" => "India",
-                "city" => "Delhi",
-                "address" => "Delhi,india",
-                "phone" => "965546885",
-            ]
-        ]);
+        $json = File::get(path: 'database/json/students.json');
+        $students = collect(json_decode($json));
 
         $students->each(function ($student) {
-            Student::create($student);
+            Student::create([
+                'name' => $student->name,
+                'email' => $student->email,
+                'age' => $student->age,
+                'gender' => $student->gender,
+                'country' => $student->country,
+                'city' => $student->city,
+                'address' => $student->address,
+                'phone' => $student->phone,
+            ]);
         });
     }
 }
